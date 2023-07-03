3042d1b1cd0f2ba54553208b8c760e8118de0604
why-not-try-calmer <mrnycticorax@gmail.com>
2023-07-03 10:35:07 +0200

    ----

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Press 'b' to configure and build, 'c' to compile and link, 'bc' to configure and compile, or 'e' to exit."
+echo "Press 'c' to configure and build dependencies, 'b' to build the application, 'cb' to configure and build the application, or 'e' to exit."
 read -e choice
 case "$choice" in
-    "b" ) cmake -S . -B build -G Ninja -D CMAKE_CXX_COMPILER=/usr/bin/g++ -D CMAKE_C_COMPILER=/usr/bin/gcc -D CMAKE_MAKE_PROGRAM=/usr/bin/ninja -D CMAKE_INSTALL_LIBDIR=/lib -D CMAKE_BUILD_DIR=/opt/builder/build -D CMAKE_BUILD_TYPE=Release -D WITH_VCPKG=ON -D SYSTEM_QT=ON -D CMAKE_INSTALL_PREFIX=/usr ;;
-    "c" ) cmake --build build --config Release ;;
-    "bc") 
+    "c" ) cmake -S . -B build -G Ninja -D CMAKE_CXX_COMPILER=/usr/bin/g++ -D CMAKE_C_COMPILER=/usr/bin/gcc -D CMAKE_MAKE_PROGRAM=/usr/bin/ninja -D CMAKE_INSTALL_LIBDIR=/lib -D CMAKE_BUILD_DIR=/opt/builder/build -D CMAKE_BUILD_TYPE=Release -D WITH_VCPKG=ON -D SYSTEM_QT=ON -D CMAKE_INSTALL_PREFIX=/usr ;;
+    "b" ) cmake --build build --config Release ;;
+    "cb") 
         cmake -S . -B build -G Ninja -D CMAKE_CXX_COMPILER=/usr/bin/g++ -D CMAKE_C_COMPILER=/usr/bin/gcc -D CMAKE_MAKE_PROGRAM=/usr/bin/ninja -D CMAKE_INSTALL_LIBDIR=/lib -D CMAKE_BUILD_DIR=/opt/builder/build -D CMAKE_BUILD_TYPE=Release -D WITH_VCPKG=ON -D SYSTEM_QT=ON -D CMAKE_INSTALL_PREFIX=/usr;
         cmake --build build --config Release;;
     "a" ) exit 0;;
